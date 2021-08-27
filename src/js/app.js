4d95efb45779c1f82245b8c7b62107753a436a01
Anna <k_anna_i@mail.ru>
2021-08-28 00:04:21 +0300

    object-curly-newline

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,8 @@
 export default function specialSkills(obj) {
   const result = [];
   obj.special.forEach((item) => {
-    const {
-      id, name, icon, description = 'Описание недоступно',
-    } = item;
-    result.push({
-      id, name, icon, description,
-    });
+    const { id, name, icon, description = 'Описание недоступно' } = item;
+    result.push({ id, name, icon, description });
   });
   return result;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,18 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
+  output: { path: path.resolve(__dirname, 'dist') },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: { loader: 'babel-loader' },
       },
       {
         test: /\.html$/,
         use: [
-          {
-            loader: 'html-loader',
-          },
+          { loader: 'html-loader' },
         ],
       },
       {
